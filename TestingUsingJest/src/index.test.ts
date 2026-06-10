22172ce6f85dd21ab3979aab526721750fcdc8c4
harshgupta751 <harshachieve300@gmail.com>
2026-06-10 17:45:29 +0530

    Testing using Jest completed

---
+++ TestingUsingJest/src/index.test.ts
@@ -0,0 +1,33 @@
+import {it, describe, expect} from '@jest/globals'
+import {sum, multiply} from './index';
+
+describe("Sum function testing", ()=>{
+
+    it("positive input testing", ()=>{
+        const finalAnswer = sum(1,3);
+
+        expect(finalAnswer).toBe(4);
+
+
+    })
+
+    it("Negative Input Testing", ()=>{
+        const finalAnswer = sum(-9, -53);
+
+        expect(finalAnswer).toBe(-62);
+    })
+
+});
+
+describe("Multiple Function testing", ()=>{
+
+it("Zero input testing", ()=>{
+const finalAnswer = multiply(0, 5);
+
+expect(finalAnswer).toBe(0);
+
+
+})
+
+
+})
---
+++ TestingUsingJest/src/index.ts
@@ -0,0 +1,13 @@
+export const sum = function(a: number, b: number){
+
+return a + b; 
+}
+
+
+export const multiply = function(a: number, b: number){
+return a * b;
+
+}
+
+console.log(sum(4,9));
+console.log(multiply(7,3));
